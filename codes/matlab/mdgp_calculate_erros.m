5e42820e3fa0cd0639cdce1dbf98ed904af4e213
michael <souza.michael@gmail.com>
2017-01-12 18:08:26 -0200

    Add parameter dij_tol

--- codes/matlab/mdgp_calculate_erros.m
+++ codes/matlab/mdgp_calculate_erros.m
@@ -1,13 +1,16 @@
-function max_eij = mdgp_calculate_erros(G, x, verbose)
-	if nargin < 3
+function max_eij = mdgp_calculate_erros(G, x, dij_tol, verbose)
+	if nargin < 4
 		verbose = false;
-	end
+    end
+    if nargin < 3
+        dij_tol = 0;
+    end
 	max_eij = 0.0;
 	for k = 1:G.nedges
 		i = G.i(k);
 		j = G.j(k);
-		lij = G.l(k);
-		uij = G.u(k);
+		lij = (1-dij_tol) * G.l(k);
+		uij = (1+dij_tol) * G.u(k);
 		xi = x(i,:);
 		xj = x(j,:);
 		dij = norm(xi - xj);

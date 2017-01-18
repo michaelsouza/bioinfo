bf1e884d2d83b49583e91f10ea0eadfd1bedac71
michael <souza.michael@gmail.com>
2017-01-18 18:06:00 -0200

    Using jacobian format

--- codes/matlab/numdiff.m
+++ codes/matlab/numdiff.m
@@ -1,12 +1,26 @@
 function g = numdiff(f, x)
 fx = f(x);
-[nrows,ncols] = size(fx);
-g = zeros(nrows,ncols,length(x));
+nfun = length(fx);
 d = zeros(size(x));
-h = 0.001;
+h = 0.0001;
+g = zeros(nfun, length(x));
 for k = 1:length(x)
     d(k) = h * max(abs(x(k)), 1.0);
     fx = f(x + d) - f(x - d);
-    g(:,:,k) = fx / (2 * d(k));
+    g(:,k) = fx / (2 * d(k));
     d(k) = 0.0;
+end
+if nfun == 1
+    % gradient
+    g = g';
+end
+end
+
+function diff()
+end
+
+function grad()
+end
+
+function jacobian()
 end
